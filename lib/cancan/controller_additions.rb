b2edb524a3352a345b3dde6c6a03446f611a38af
Patrick Ritchie <pritchie@gmail.com>
2011-04-29 09:35:21 -0400

    fix ThreadJumpError when using cancan with warden

--- lib/cancan/controller_additions.rb
+++ lib/cancan/controller_additions.rb
@@ -247,10 +247,11 @@ module CanCan
       #
       def check_authorization(options = {})
         self.after_filter(options.slice(:only, :except)) do |controller|
-          return if controller.instance_variable_defined?(:@_authorized)
-          return if options[:if] && !controller.send(options[:if])
-          return if options[:unless] && controller.send(options[:unless])
-          raise AuthorizationNotPerformed, "This action failed the check_authorization because it does not authorize_resource. Add skip_authorization_check to bypass this check."
+          if !controller.instance_variable_defined?(:@_authorized) &&
+             !(options[:if] && !controller.send(options[:if])) &&
+             !(options[:unless] && controller.send(options[:unless]))
+            raise AuthorizationNotPerformed, "This action failed the check_authorization because it does not authorize_resource. Add skip_authorization_check to bypass this check."
+          end
         end
       end
 
